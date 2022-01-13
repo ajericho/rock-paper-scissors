f50126ea789d3c842e0dce142ab4a0e2d957c6e4
Amanda Jericho <ajericho@gmail.com>
2022-01-13 11:35:22 -0500

    added score count

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Game from './components/Game';
 import './App.css';
 
 function App() {
-  const [gameCount, setGameCount] = useState(0);
-
-  function increaseScore() {
-    setGameCount((gameCount) => (
-      gameCount += 1
-    ))
-  }
 
 
   return (
 
     <div className="App">
       <header className="App-header">
-        <p>Game Count: {gameCount}</p>
+
       </header>
       <main>
-
-            <Game score={increaseScore}/>
-
+        <Game />
       </main>
     </div>
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Results from '../components/Results';
 import Playing from '../components/Playing';
 
-export default function Game({ score }) {
+export default function Game() {
     const [playerChoice, setPlayerChoice] = useState(null);
     const [computerChoice, setComputerChoice] = useState(null);
     const [isResults, setIsResults] = useState(false);
-    const [playerWins, setPlayerWins] = useState(null);
+
+    const winner = useRef(null)
+    const score = useRef(0);
 
 
     const options = ['rock', 'paper', 'scissors'];
-
 
     //function to randomly choose computers option
     function drawRandom() {
@@ -19,28 +20,28 @@ export default function Game({ score }) {
     }
 
     //function to determine winner
-
     function determineWinner() {
         if (
             (playerChoice === 'rock' && computerChoice === 'rock') ||
             (playerChoice === 'paper' && computerChoice === 'paper') ||
             (playerChoice === 'scissors' && computerChoice === 'scissors')
         ) {
-             setPlayerWins('Tie!')
+            return winner.current = 'Tie!'
         }
         else if (
             (playerChoice === 'rock' && computerChoice === 'scissors') ||
             (playerChoice === 'paper' && computerChoice === 'rock') ||
             (playerChoice === 'scissors' && computerChoice === 'paper')
-            
+
         ) {
-             setPlayerWins('Player Wins!')
+            score.current += 1;
+            return winner.current = 'Player Wins!'
+
         }
         else {
-             setPlayerWins('House Wins!')
+            return winner.current = 'House Wins!'
         }
     }
-
 
 
     function playGame(val) {
@@ -48,30 +49,24 @@ export default function Game({ score }) {
         setPlayerChoice(val);
         //execute computers random choice function
         setComputerChoice(drawRandom());
-        //increase score
-        score();
         //route to result screen 
         setIsResults(true);
     }
 
-     //determine winner
-    useEffect(() => {
-        determineWinner();
-    });
 
     return (
         <div>
+            {console.log(score)}
+            <p>Score: {score.current}</p>
             {isResults === true ?
                 <Results
                     playerChoice={playerChoice}
                     computerChoice={computerChoice}
                     setResults={setIsResults}
-                    winner={playerWins} />
+                    winner={determineWinner()} />
                 :
                 <Playing options={options} playGame={playGame} />
             }
-
-
         </div>
     )
 }
